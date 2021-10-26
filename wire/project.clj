0fe958178c6f08f807a9005ad46dcc1fcca5f70d
Michael Whatcott <mdwhatcott@gmail.com>
2021-10-26 15:39:26 -0600

    Updated class for animated warning.

--- wire/project.clj
+++ wire/project.clj
@@ -1,4 +1,4 @@
-(defproject com.cleancoders.c3kit/wire "1.0.9"
+(defproject com.cleancoders.c3kit/wire "1.0.10"
 
   :description "Clean Coders Clojure (C3) Kit - Wire: Rich-client webapp tools."
   :url "https://cleancoders.com"
