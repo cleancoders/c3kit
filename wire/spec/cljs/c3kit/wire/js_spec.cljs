607de4a5e3c9dceaad60533bcdbd7b2ed51607e0
Micah <micahmartin@gmail.com>
2022-11-04 13:54:54 -0700

    removes sub projects

--- wire/spec/cljs/c3kit/wire/js_spec.cljs
+++
@@ -1,8 +0,0 @@
-(ns c3kit.wire.js-spec
-  (:require-macros [speclj.core :refer [describe context it should= should-not= after before should-contain around with]])
-  (:require [c3kit.wire.js :as sut]
-            [speclj.core]))
-
-(describe "Core"
-
-  )
--- wire/src/cljs/deps.cljs
+++
@@ -1,2 +0,0 @@
-{:libs ["c3kit/wire"]
- :externs      []}
